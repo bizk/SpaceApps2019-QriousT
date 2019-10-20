345ba465f77dac600041e4431ebc907a8f3ea55d
bizk <santiago.yanzon1999@gmail.com>
2019-10-20 00:47:55 -0300

    Added basic frontend

---
+++ FrontEnd/src/App.js
@@ -0,0 +1,104 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+import Toolbar from '@material-ui/core/Toolbar';
+import { AppBar, IconButton, Typography } from '@material-ui/core';
+import { fade, makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles(theme => ({
+  grow: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    display: 'none',
+    [theme.breakpoints.up('sm')]: {
+      display: 'block',
+    },
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(3),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    width: theme.spacing(7),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 7),
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: 200,
+    },
+  },
+  sectionDesktop: {
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'flex',
+    },
+  },
+  sectionMobile: {
+    display: 'flex',
+    [theme.breakpoints.up('md')]: {
+      display: 'none',
+    },
+  },
+}));
+
+
+function App() {
+  return (
+    <div className="App">
+      <AppBar> 
+        <Toolbar>
+          <IconButton
+            edge="start"
+            color="inherit"
+          >
+          </IconButton>
+
+        </Toolbar>
+        <Typography variant="h6"> QriusT - Helping hands</Typography>
+      </AppBar>
+      < header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React aaa
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
