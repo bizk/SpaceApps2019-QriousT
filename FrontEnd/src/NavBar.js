eb11f43118d4cf75c9503ce05d3ca7c29c6c0c34
bizk <santiago.yanzon1999@gmail.com>
2019-10-20 13:08:42 -0300

    Navbar improvements

--- FrontEnd/src/NavBar.js
+++ FrontEnd/src/NavBar.js
@@ -2,12 +2,8 @@ import React from 'react';
 import { fade, makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
-import InputBase from '@material-ui/core/InputBase';
-import SearchIcon from '@material-ui/icons/Search';
-//import AccountCircle from '@material-ui/icons/AccountCircle';
-import MoreIcon from '@material-ui/icons/MoreVert';
+import Button from '@material-ui/core/Button';
 
 import {red} from '@material-ui/core/colors';
 
@@ -47,6 +43,7 @@ export default function NavBar() {
             PVDS - Population Vulnerability Diagnosis System
           </Typography>
           <div className={classes.grow} />
+          <Button variant="contained" color="primary">Generate Diagnosis</Button>
         </Toolbar>
       </AppBar>
     </div>
